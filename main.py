45b646a9cb9e9f6a396f7ec73315c806b30e581a
Rugved <74641670+rugvedp@users.noreply.github.com>
2024-06-07 20:44:28 +0530

    delete

--- main.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-client = pymongo.MongoClient("mongodb+srv://capsule:12345@capsule.fzj2u.mongodb.net/Imp")
-
-database = client["Imp"]
-collection = database['Tees']
-all = collection.find()
-
-for idx, val in enumerate(all):
-    print(val['name'])
